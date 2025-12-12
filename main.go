772de340da595849e23979d82a54699a8c5b0bbc
hakem26 <hamed26km@gmail.com>
2025-12-12 14:11:09 +0000

    add main

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	_, _ = w.Write([]byte("<h1>Welcome to the Home Page</h1>"))
+	
+}
 
 func main()  {
-	fmt.Println("Hello")
+	mux := http.ListenAndServe(":8080", http.HandlerFunc(home))
+	if err := mux; err != nil {
+		log.Fatal(err)
+	}
 }

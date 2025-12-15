867f6f29bb929ce3c2c9bad768453d4a1f2f8ed6
hakem26 <hamed26km@gmail.com>
2025-12-15 00:53:53 +0000

    01

--- 01-simple-http/main.go
+++ 01-simple-http/main.go
@@ -4,46 +4,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 )
 
-type User struct {
-	Name string `json:"name"`
-	Age int `json:"age"`
-	CreaedAt time.Time `json: "created_at"`
-}
-
-type getUser interface {
-	GetUsers() []User
-}
-
-func getUsers() []User {
-	return []User {
-		{Name: "Alice", Age: 30, CreaedAt: time.Now()},
-		{Name: "Bob", Age: 25, CreaedAt: time.Now()},
-	}
-}
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	if r.URL.Path != "hello" {
+	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
 	}
-	w.Header().Set("Content-type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("<h1>Hello World!</h1>"))
+	fmt.Fprintf(w, "Hello World!")
 }
+
 func formHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	if r.ParseForm() != nil {
-		http.Error(w, "Bad request", http.StatusBadRequest)
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, "Error parsing form", http.StatusBadRequest)
 		return
 	}
 	fmt.Fprintf(w, "Form submitted successfully!\n")
